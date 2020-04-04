439da3ee0476612bab4519f0711f4bb2f51b0d8a
Víctor Ceballos Espinosa <victcebesp@gmail.com>
2020-04-04 17:55:13 +0100

    Add new lambda function to dynamically update graphs limits

---
+++ maximumEncodedDayHandler.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import json
+
+def get_maximum_values(event, context):
+    data = pd.read_csv('s3://covid-visualization/representative.csv')
+    maximum_encoded_day = data['EncodedDay'].max()
+    maximum_confirmed_cases = data['Confirmations'].max()
+    return {
+        "statusCode": 200,
+        "headers": {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': 'true',
+        },
+        'body': json.dumps({
+                                "maximum_encoded_day": str(maximum_encoded_day),
+                                "maximum_confirmed_cases": str(maximum_confirmed_cases)
+                           })
+    }
+
+if __name__ == "__main__":
+    get_maximum_values('', '')
